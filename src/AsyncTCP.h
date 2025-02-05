2b8988b379bdf95ff5c8d268222481620589ff53
Will Miles <will@willmiles.net>
2025-02-05 21:54:59 +0000

    Explicitly indicate non-copyability

--- src/AsyncTCP.h
+++ src/AsyncTCP.h
@@ -80,6 +80,14 @@ public:
   AsyncClient(tcp_pcb *pcb = 0);
   ~AsyncClient();
 
+  // Noncopyable
+  AsyncClient(const AsyncClient &) = delete;
+  AsyncClient &operator=(const AsyncClient &) = delete;
+
+  // Nonmovable
+  AsyncClient(AsyncClient &&) = delete;
+  AsyncClient &operator=(AsyncClient &&) = delete;
+
   inline bool operator==(const AsyncClient &other) {
     return _pcb == other._pcb;
   }
@@ -289,6 +297,15 @@ public:
 #endif
   AsyncServer(uint16_t port);
   ~AsyncServer();
+
+  // Noncopyable
+  AsyncServer(const AsyncServer &) = delete;
+  AsyncServer &operator=(const AsyncServer &) = delete;
+
+  // Nonmovable
+  AsyncServer(AsyncServer &&) = delete;
+  AsyncServer &operator=(AsyncServer &&) = delete;
+
   void onClient(AcConnectHandler cb, void *arg);
   void begin();
   void end();
